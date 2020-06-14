f39d63f73be41f9115e21d975843962dc21356b4
akpante3 <obije.victor5@gmail.com>
2020-06-14 23:23:38 +0100

    about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import {
   Link
 } from "react-router-dom";
+import './About.scss'
 
 const About = (props) => {
 
   return (
     <div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/1YuN3POoNnQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div className="about">
+        <div className="about__item">
+          <div>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/1YuN3POoNnQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <div>
+              <div className="about__header"> The power to create </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
