334228f16e83c4f394632f33f063bd378f74ce02
akpante3 <obije.victor5@gmail.com>
2020-06-21 13:18:21 +0100

    founders img

--- src/components/team/Founders.jsx
+++ src/components/team/Founders.jsx
@@ -1,13 +1,63 @@
 import React from 'react';
-import './Team.scss'
 
 const Founders = (props) => {
+   const foundersList = [
+      {
+        firstName: 'Chikodi',
+        lastName: 'Ukaiwe',
+        image: require('../../icons/images/chikodi-ukaiwe.png'),
+        role: 'Partner'
+      },
+      {
+        firstName: 'Seye',
+        lastName: 'Bandele',
+        image: require('../../icons/images/seye-bandele.png'),
+        role: 'Branding Partner'
+      },
+      {
+        firstName: 'Ope',
+        lastName: 'Adetomiwa',
+        image: require('../../icons/images/ope-adetomiwa.png'),
+        role: 'Communications/Partner'
+      },
+      {
+        firstName: 'Joshua',
+        lastName: 'Biyere',
+        image: require('../../icons/images/joshua-biyere.png'),
+        role: 'Design Partner'
+      },
+
+   ]
+
+
+
+  const foundersDetails = (data) => {
+    return (
+      <div className="founders__image">
+      <div className="founders__image__text">
+        <div className="founders__image__text__name"><h1>{data.firstName}</h1><h1>{data.lastName}</h1></div>
+        <div className="founders__image__text__role">{data.role}</div>
+      </div>
+      <img src={data.image}/>
+    </div>
+    )
+  }
+
+
   return (
       <div className="founders">
-         <div>
-         <div className={['philosophy__text__header'].join(' ')}>
-              Philosophy
-          </div>
+          <div className="founders__text">
+            <div>
+                <div className={['founders__text__header'].join(' ')}>
+                  The Crew
+                </div>
+            </div>
+            <p className='founders__text__body'>
+              We’re FORZA, a marketing agency.We work with goal oriented brands and companies to create digital marketing solutions that people love. Because what works for people, works for business.
+            </p>
+         </div>
+         <div className="founders__images">
+           { foundersList.map((data) => foundersDetails(data)) }
          </div>
       </div>
   );
--- src/pages/team/Team.jsx
+++ src/pages/team/Team.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Team.scss'
+import Founders from '../../components/team/Founders';
 
 const Team = (props) => {
     // const { previous, next, bg } = props
   return (
       <div className="team">
         <div className="team__founders">
-
+          <Founders />
         </div>
         <div className="team__team">
              
