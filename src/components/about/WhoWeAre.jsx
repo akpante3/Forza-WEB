d17f1c3d0664dd1d4d79e85d96afb33662a2f1d8
akpante3 <obije.victor5@gmail.com>
2020-07-27 23:32:35 +0100

    who we arew update

--- src/components/about/WhoWeAre.jsx
+++ src/components/about/WhoWeAre.jsx
@@ -13,16 +13,7 @@ const WhoWeAre = (props) => {
   return (
       <div className="who-we-are" ref={ref}>
         <SideNav visible={visible} list={ aboutNavList } />
-        <div className="who-we-are__item-container">
-          <div className="who-we-are__item who-we-are__video">
-              <iframe width="460" height="415" src="https://www.youtube.com/embed/1YuN3POoNnQ" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-          </div>
-          <div className="who-we-are__item">
-              <div className="who-we-are__header"> <div>The <span>p<div className="who-we-are__power-icon"><NavIcon /></div>wer</span></div> <div>to create</div> </div>
-              <p>We are Forza, a result-oriented marketing communications agency. We work with brands and businesses to create marketing solutions that people love. Because what works for people, works for business.</p>
-              <p>With our robust portfolio and in-house capacity in brand & identity development, design & creation, communications & public relations, digital marketing, content production, strategic consulting and social media/community management, Forza delivers fresh contemporary marketing and advertising services to individuals, MSMEs and large corporations.</p>
-          </div>
-        </div>
+        <iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/1YuN3POoNnQ?modestbranding=1&autohide=1&showinfo=0&controls=0" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
   );
 }
