883043e6c669c4435e29dee5d6a044a5ce3e7512
akpante3 <obije.victor5@gmail.com>
2020-07-29 22:41:01 +0100

    home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -29,46 +29,52 @@ const Home = () => {
 
     return (
       <div>
-      <div className={['home', isDay ? '' : 'home--dark-theme' ].join(' ')} >
-        <div className="home__text">
-          <div>
-              <div className="home__header__wrapper">
-                <div className={['home__header', 'home_pd', 'border-left', 'header' ].join(' ')}><div className="home__header__animate">{switchText}</div></div>
+        <div className="home-text">
+           <p>Design</p>
+           <p>that</p>
+           <p>works</p>
+           <p>
+                GET TO KNOW US
+              </p>
+        </div>
+        <div className="home-header">
+          <img src={require('../../icons/images/homepagephoto.png')} />
+        </div>
+        <div className={['home', isDay ? '' : 'home--dark-theme' ].join(' ')} >
+          <div className="home__images">
+                <div className="">
+                {
+                  isDay ? (<div className="home__images__theme" onClick={ () => toggleTheme() }><Sun /></div>) : (<div className="home__images__theme" onClick={ () => toggleTheme() }><Moon /></div>)
+                }
+                </div>
+                <div className="home__images__bird-one">
+                  <Bird />
+                </div>
+                <div className="home__images__cloud-top">
+                    <CloudTop />
+                </div>
+                <div className="home__images__cloud">
+                  <Cloud />
+                </div>
+              <div className="home__images__building">
+                <Building />
               </div>
-              <div className="home__header home_pd">that</div>
-              <div className="home__header home_pd">works</div>
-              <span className="home__link home_pd">GET TO KNOW US</span>
-          </div>
-          <div className="home__decription home_pd">
-              <div>Let’s work with you to</div>
-              <div>get desired results.</div>
           </div>
         </div>
-        <div className="home__images">
-              <div className="">
-              {
-                isDay ? (<div className="home__images__theme" onClick={ () => toggleTheme() }><Sun /></div>) : (<div className="home__images__theme" onClick={ () => toggleTheme() }><Moon /></div>)
-              }
-              </div>
-              <div className="home__images__bird-one">
-                <Bird />
-              </div>
-              <div className="home__images__cloud-top">
-                  <CloudTop />
-              </div>
-              <div className="home__images__cloud">
-                <Cloud />
-              </div>
-            <div className="home__images__building">
-              <Building />
+        <div className="home-footer">
+          <div className="home-footer__lets-talk">
+            <LetsTalk />
+            <div className="home-footer__lets-talk__text">
+              <p>Find out more about how we can</p>
+              <p>work together</p>
             </div>
+            <div className="home-footer__lets-talk__text__link">
+              <p>
+                START A PROJECT
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="home-footer">
-        <div>
-           <LetsTalk />
-        </div>
-      </div>
       </div>
     );
 }
