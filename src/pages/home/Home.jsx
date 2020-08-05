d2328ec14e5b48919ba971d7e3c5f7f794b816aa
akpante3 <obije.victor5@gmail.com>
2020-08-05 03:25:20 +0100

    home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,45 +8,71 @@ import { ReactComponent as Bird } from '../../icons/bird.svg';
 import { ReactComponent as Moon } from '../../icons/moon.svg';
 import { ReactComponent as Sun } from '../../icons/sun.svg';
 import { ReactComponent as LetsTalk } from '../../icons/letsTalk.svg';
+import { useOnScreen } from '../../hooks/index';
+
+const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop) 
 
 const Home = () => {
-    const [ switchText, setSwitchText ] = useState('Branding')
+    const [ ref, visible ] = useOnScreen({ threshold: 0.5 })
+    const [ headerRef, HeaderVisible ] = useOnScreen({ threshold: 0.80 })
+    const [ switchText, setSwitchText ] = useState('Services')
     const [ isDay, setIsDay ]= useState( true )
+    const [ coverImage, setCoverImage ] = useState('https://firebasestorage.googleapis.com/v0/b/forza-42793.appspot.com/o/62DDFA63-EB83-4214-80BC-204EFCC0BDD4.png?alt=media&token=b74b31e5-5583-4c89-848b-f5efbe9544e1')
     const headerWords = ['Branding', 'Marketing']
+    const coverImageList = ['https://firebasestorage.googleapis.com/v0/b/forza-42793.appspot.com/o/62DDFA63-EB83-4214-80BC-204EFCC0BDD4.png?alt=media&token=b74b31e5-5583-4c89-848b-f5efbe9544e1', 'https://firebasestorage.googleapis.com/v0/b/forza-42793.appspot.com/o/Screen%20Shot%202020-08-04%20at%201.03.16%20AM.png?alt=media&token=a7817bff-96b5-488e-bc21-0fd0a4d9bfca']
+    let [ counter, setCounter ] = useState(0)
+    const executeScroll = (ref) => scrollToRef(ref)
     
 
-    const textSwitch = () => (setInterval(() => {
+    const textSwitch = () => {
         let number = Math.floor((Math.random() * 2) + 0);
-        setSwitchText(headerWords[number])
-    }, 3000))
+        
+    }
+
+    //#endregion
+    useEffect(() => { 
+      if(HeaderVisible) {
+        executeScroll(headerRef);
+      }
+    }, [ HeaderVisible ])
 
     const toggleTheme = () => {
       setIsDay(!isDay)
     }
 
-    useEffect(() => {
-      textSwitch()
-    }, []);
+    const AfterType = () => {
+      setSwitchText(headerWords[counter])
+      console.log(counter, coverImage)
+      setCoverImage(coverImageList[counter])
+      if ( counter == headerWords.length - 1) {
+        setCounter(0)
+      } else {
+        counter++
+        setCounter(counter)
+      }
+    }
+
+    const image = () => (<img ref={headerRef} className="home-header__cover-image" src={coverImage} style={{ transition: '1s'}}/>)
 
     return (
       <main className={['home', isDay ? '' : 'home--dark-theme' ].join(' ')}>
         <div className="home-text-headers">
            <p className="home-text-headers__text home-text-headers__bold">
-           Design
-           {/* <Typing>
-              <span>Design</span>
-              <Typing.Backspace count={20} />
-            </Typing> */}
+{   HeaderVisible ?        (<Typing onFinishedTyping={() => AfterType()} loop={true} speed={300}>
+              <span>{switchText}</span>
+              <Typing.Backspace count={switchText.length} />
+            </Typing>) : switchText}
             </p>
            <p className="home-text-headers__text home-text-headers__medium">that</p>
            <p className="home-text-headers__text home-text-headers__medium">works</p>
            <p className="home-text-headers__link">GET TO KNOW US</p>
         </div>
         <div className="home-header">
-          <img src="https://firebasestorage.googleapis.com/v0/b/forza-42793.appspot.com/o/Screen%20Shot%202020-08-04%20at%201.03.16%20AM.png?alt=media&token=a7817bff-96b5-488e-bc21-0fd0a4d9bfca"/>
+           { image() }
+           <img onClick={() => executeScroll(ref)} className="home-header__direction-icon" src={require('../../icons/downward-arrow.png')} />
         </div>
-        <div className="home-body">
-          <div className="home-text-body">
+        <div className="home-body" ref={ref}>
+          <div className={["home-text-body", visible? 'home-text-body--animate-in' : 'home-text-body--animate-out'].join(' ')}>
               We’re a marketing agency. We work with goal oriented brands and companies to create digital marketing solutions that people love. Because what works for people, works for business.
           </div>
           <div className="home__images">
