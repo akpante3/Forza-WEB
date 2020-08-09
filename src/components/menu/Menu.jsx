8dff092be3327761004abd090abaecc9c7102122
akpante3 <obije.victor5@gmail.com>
2020-08-09 21:42:04 +0100

    menu update

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import {
-  Link
+  Link, useLocation
 } from "react-router-dom";
 import './Menu.scss';
 
 const Menu = (props) => {
+  let location = useLocation()
 
   return (
     <div className={['menu','menu--bg-white', `${props.class}`].join(' ')}>
        <div className="menu__items">
-            <div className="menu__item" onClick={props.toggleMenu}><Link to="/">Home</Link></div>
-            <div className="menu__item" onClick={props.toggleMenu}><Link to="/about/who-we-are">About Us</Link></div>
-            <div className="menu__item" onClick={props.toggleMenu}><Link to="/services/our-services">Our Services</Link></div>
-            <div className="menu__item" onClick={props.toggleMenu}><Link to="/our-works">Our Works</Link></div>
-            <div className="menu__item" onClick={props.toggleMenu}><Link to="/team">The Team</Link></div>
-            <div className="menu__item" onClick={props.toggleMenu}><Link to="/contact">Contact Us</Link></div>
+            <div className={["menu__item", location.pathname === '/' ? "menu__item--active" : ''].join(' ')} onClick={props.toggleMenu}><Link to="/">Home</Link></div>
+            <div className={["menu__item", location.pathname.includes('/about') ? "menu__item--active" : ''].join(' ')} onClick={props.toggleMenu}><Link to="/about/who-we-are">About Us</Link></div>
+            <div className={["menu__item", location.pathname.includes('/services/') ? "menu__item--active" : ''].join(' ')} onClick={props.toggleMenu}><Link to="/services/our-services">Our Services</Link></div>
+            <div className={["menu__item", location.pathname === '/our-work' ? "menu__item--active" : ''].join(' ')} onClick={props.toggleMenu}><Link to="/our-works">Our Work</Link></div>
+            <div className={["menu__item", location.pathname === '/team' ? "menu__item--active" : ''].join(' ')} onClick={props.toggleMenu}><Link to="/team">The Team</Link></div>
+            <div className={["menu__item", location.pathname === '/contact' ? "menu__item--active" : ''].join(' ')} onClick={props.toggleMenu}><Link to="/contact">Contact Us</Link></div>
        </div>
     </div>
   );
