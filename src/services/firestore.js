f06834c6fc9f5181b9fa9f679d74e97d043e92de
akpante3 <obije.victor5@gmail.com>
2020-08-09 19:00:39 +0100

    hide keys

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -2,14 +2,14 @@ import * as firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyApbEXKTeko5YsuCQ1OMnzwl2I91aY7ak4",
-    authDomain: "forza-42793.firebaseapp.com",
-    databaseURL: "https://forza-42793.firebaseio.com/",
-    projectId: "forza-42793",
-    storageBucket: "forza-42793.appspot.com",
-    messagingSenderId: "170754823498",
-    appId: "1:170754823498:web:c21bc65a6c3ed720f21db3",
-    measurementId: "G-QEJJN86ZX9"
+    // apiKey: "AIzaSyApbEXKTeko5YsuCQ1OMnzwl2I91aY7ak4",
+    // authDomain: "forza-42793.firebaseapp.com",
+    // databaseURL: "https://forza-42793.firebaseio.com/",
+    // projectId: "forza-42793",
+    // storageBucket: "forza-42793.appspot.com",
+    // messagingSenderId: "170754823498",
+    // appId: "1:170754823498:web:c21bc65a6c3ed720f21db3",
+    // measurementId: "G-QEJJN86ZX9"
 };
 
 firebase.initializeApp(firebaseConfig);
